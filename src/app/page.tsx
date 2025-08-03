f4b4eadd91a29f454e554b0b5fb24704d3d5e441
yukiyuki925 <yuki.work1999@gmail.com>
2025-08-03 09:01:39 +0900

    チェックつけたらタスク消す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { TaskType } from "@/types";
 
 async function fetchAllTasks() {
@@ -5,12 +8,29 @@ async function fetchAllTasks() {
     cache: "no-store",
   });
   const data = await res.json();
-  console.log(data);
   return data.tasks;
 }
 
-export default async function Home() {
-  const tasks = await fetchAllTasks();
+const handleChange = async (
+  task: TaskType,
+  setTasks: (tasks: TaskType[]) => void
+) => {
+  await fetch(`http://localhost:3000/api/task/${task.id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ done: !task.done }),
+  });
+  const updated = await fetchAllTasks();
+  setTasks(updated);
+};
+
+export default function Home() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  useEffect(() => {
+    fetchAllTasks().then(setTasks);
+  }, []);
+
   return (
     <>
       <div className="bg-white py-6 sm:py-8 lg:py-12">
@@ -20,7 +40,6 @@ export default async function Home() {
               Task
             </h2>
           </div>
-
           <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
             {tasks.map((task: TaskType) => (
               <div
@@ -28,17 +47,26 @@ export default async function Home() {
                 className="flex flex-col overflow-hidden rounded-lg border bg-white"
               >
                 <div className="flex flex-1 flex-col p-4 sm:p-6">
-                  <h2 className="mb-2 text-lg font-semibold text-gray-800">
-                    <a
-                      href="#"
-                      className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
-                    >
-                      {task.title}
-                    </a>
-                  </h2>
-
+                  <div className="flex items-center justify-between">
+                    <h2 className="mb-2 text-lg font-semibold text-gray-800">
+                      <a
+                        href="#"
+                        className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+                      >
+                        {task.title}
+                      </a>
+                    </h2>
+                    <div>
+                      <input
+                        type="checkbox"
+                        checked={task.done}
+                        onChange={() => handleChange(task, setTasks)}
+                      />
+                    </div>
+                  </div>
                   <div className="mt-auto flex items-end justify-between">
                     <div className="flex items-center gap-2">
+                      <span className="text-sm text-gray-400">期限</span>
                       <div>
                         <span className="block text-sm text-gray-400">
                           {new Date(task.createdAt).toLocaleString("ja-JP", {
